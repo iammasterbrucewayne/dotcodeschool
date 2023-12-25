63c5cf47d09cfb574bb854af8305c4f983d38074
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2023-12-25 20:57:47 +0530

    bug fix: fix progress override issue

--- src/pages/api/update-progress.ts
+++ src/pages/api/update-progress.ts
@@ -8,10 +8,23 @@ export default async function handler(req: any, res: any) {
 
   const operations = updates.map((update: any) => {
     const { user, progress } = update;
+    const updateObject = {};
+    for (const courseId in progress) {
+      console.log(courseId);
+      for (const lessonId in progress[courseId]) {
+        console.log(lessonId);
+        for (const chapterId in progress[courseId][lessonId]) {
+          console.log(chapterId);
+          updateObject[`progress.${courseId}.${lessonId}.${chapterId}`] =
+            progress[courseId][lessonId][chapterId];
+        }
+      }
+    }
+    console.log("updateObject", JSON.stringify(updateObject, null, 2));
     return {
       updateOne: {
         filter: { email: user.email },
-        update: { $set: { progress } },
+        update: { $set: updateObject },
       },
     };
   });
